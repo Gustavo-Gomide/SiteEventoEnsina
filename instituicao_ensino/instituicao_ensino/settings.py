092aeef320d35e78c939c55f9fa4c1c6f28fcc21
madmattp <mpqfreitas@gmail.com>
2025-11-24 00:24:34 +0000

    adicionando middleware de auditoria ao settings.py

--- instituicao_ensino/instituicao_ensino/settings.py
+++ instituicao_ensino/instituicao_ensino/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "instituicao_ensino.middleware.AuditMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
